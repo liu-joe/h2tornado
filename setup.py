d88d636eb209f934e85e2b81fee61176259228e7
Andrew Huang <k.andrew.huang@gmail.com>
2019-02-13 09:23:20 -0500

    Add requirements, setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split("\n")
+
+config = {
+    'description': 'Tornado-based QProxy Client',
+
+    'url': 'https://github.com/ContextLogic/h2tornado',
+    'author': 'Andrew Huang',
+    'author_email': 'andrew@wish.com',
+    'license': 'MIT',
+     'classifiers': [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    'version': '0.0.1',
+    'packages': ['h2tornado'],
+    'scripts': [],
+    'name': 'h2tornado',
+    # TODO: unify with requirements.txt
+    'install_requires': install_requires,
+}
+
+setup(**config)
