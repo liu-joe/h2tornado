b170bfb75d623086e83d4c51b1818cacdf94d284
Andrew Huang <k.andrew.huang@gmail.com>
2019-02-18 16:05:19 -0800

    Revert "Set self._closed = False when reconnected in H2Connection"
    This reverts commit 337e44f4087baf00af17384e1efe76c464b20b6c.

--- h2tornado/connection.py
+++ h2tornado/connection.py
@@ -315,7 +315,6 @@ class H2Connection(object):
                             cancelled),
                         callback=read_until_cancelled)
 
-            self._closed = False
             read_until_cancelled()
             self.flush()
         except Exception as e:
